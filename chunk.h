ef71d86e6bb0ae4b8f0d22046f692411f7e058d4
Nicholas Turon <nicholas.turon@joinroot.com>
2023-09-11 18:31:22 -0400

    chapter 15

--- chunk.h
+++ chunk.h
@@ -6,6 +6,12 @@
 
 typedef enum {
     OP_CONSTANT,
+    OP_ADD,
+    OP_SUBTRACT,
+    OP_MULTIPLY,
+    OP_DIVIDE,
+    OP_MODULO,
+    OP_NEGATE,
     OP_RETURN,
 } OpCode;
 
--- main.c
+++ main.c
@@ -1,17 +1,36 @@
 #include "common.h"
+#include "vm.h"
 #include "chunk.h"
 #include "debug.h"
 
 int main(int argc, const char* argv[]) {
+  initVM();
+
   Chunk chunk;
   initChunk(&chunk);
   
-  int index = addConstant(&chunk, 1.2);
+  int constant_pointer = addConstant(&chunk, 1.2);
   writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, index, 123);
+  writeChunk(&chunk, constant_pointer, 123);
+
+  constant_pointer = addConstant(&chunk, 3.4);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant_pointer, 123);
+
+  writeChunk(&chunk, OP_ADD, 123);
+
+  constant_pointer = addConstant(&chunk, 5.6);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant_pointer, 123);
+
+  writeChunk(&chunk, OP_DIVIDE, 123);
+  writeChunk(&chunk, OP_NEGATE, 123);
+
   writeChunk(&chunk, OP_RETURN, 123);
   
   disassembleChunk(&chunk, "test chunk");
+  interpret(&chunk);
+  freeVM();
   freeChunk(&chunk);
   return 0;
 }
